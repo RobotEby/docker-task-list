3fa3a08f2ca7742271e96e18c9f2a612e241e278
Kerlon Amaral <ubuntu.onion@hotmail.com>
2026-01-21 23:06:56 -0300

    feat: enhance CORS configuration for API

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/todolist';
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://docker-task-list.vercel.app', 'http://localhost:8080'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'userid'],
+  }),
+);
 app.use(json());
 
 connect(mongoURI)
