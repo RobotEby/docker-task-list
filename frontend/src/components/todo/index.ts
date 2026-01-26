5f11bc2dc4f0d6654e987a87bf1b7f44e4b85059
robotEby <ubuntu.onion@hotmail.com>
2026-01-26 17:02:51 -0300

    feat: export core todo components for reuse

--- frontend/src/components/todo/index.ts
+++ frontend/src/components/todo/index.ts
@@ -2,3 +2,4 @@ export { TodoInput } from './TodoInput';
 export { TodoItem } from './TodoItem';
 export { TodoList } from './TodoList';
 export { StatsPanel } from './StatsPanel';
+export { SortableTodoItem } from './SortableTodoItem';
